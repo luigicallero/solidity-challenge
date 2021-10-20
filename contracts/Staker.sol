9999427a25ecc67757e29fbba7ebbcd1a31bea2d
luigicallero <luigicallero@gmail.com>
2021-10-20 09:27:43 -0300

    All tokens migrated to Staker contract on deployment

---
+++ contracts/Staker.sol
@@ -0,0 +1,14 @@
+// contracts/Staker.sol
+/* SPDX-License-Identifier: GPL-3.0
+Authored by Luis Ignacio Callero
+*/
+
+pragma solidity ^0.8.4;
+
+import "./RewardToken.sol";
+
+contract Staker {
+    string public name = "Luisca Staking Application";
+    RewardToken public RWT;
+
+}
--- migrations/2_rewardToken_migration.js
+++ migrations/2_rewardToken_migration.js
@@ -1,5 +1,13 @@
 const RewardToken = artifacts.require("RewardToken");
+const Staker = artifacts.require("Staker");
 
-module.exports = function (deployer) {
-  deployer.deploy(RewardToken,1000000);
+module.exports = async function (deployer) {
+    await deployer.deploy(RewardToken, '1000000000000000000000000');
+    const rwt = await RewardToken.deployed();
+
+    await deployer.deploy(Staker);
+    const staker = await Staker.deployed();
+
+    // Transfer all tokens to staking account
+    await rwt.transfer( staker.address , '1000000000000000000000000')
 };
