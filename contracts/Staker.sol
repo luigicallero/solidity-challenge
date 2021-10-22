331b8bf62730cbb84b12f8ea27d71e5a9b91e564
luigicallero <luigicallero@gmail.com>
2021-10-22 09:04:22 -0300

    Tests: RWT distributed on different accounts, Staking 50%

--- contracts/Staker.sol
+++ contracts/Staker.sol
@@ -1,24 +1,137 @@
 // contracts/Staker.sol
 /* SPDX-License-Identifier: GPL-3.0
 Authored by Luis Ignacio Callero
-With great help from Greg DappUniversity https://www.youtube.com/watch?v=sCE-fQJAVQ4
-    and Austin Griffith https://github.com/scaffold-eth/scaffold-eth/blob/staking-app/packages/hardhat/contracts/Staker.sol 
+With great help from 
+    Greg DappUniversity https://www.youtube.com/watch?v=sCE-fQJAVQ4
+    Austin Griffith https://github.com/scaffold-eth/scaffold-eth/blob/staking-app/packages/hardhat/contracts/Staker.sol
+    https://medium.com/@tnhollan/how-to-implement-staking-in-solidity-cdb1d0506ef6
 */
 
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol"; // to be used with remix
 import "./RewardToken.sol";
 
 contract Staker {
     string public name = "Luisca Staking Application";
     RewardToken public RWT;
-
-    event Stake(address staker, uint256 amount);
-
+    using SafeMath for uint256;
     uint256 public constant threshold = 1 ether;
-
     mapping ( address => uint256 ) public balances;
+    event Stake(address staker, uint256 amount);
+    /**
+     * @notice We usually require to know who are all the stakeholders.
+     */
+    address[] internal stakeholders;
+    
+    /**
+    * @notice A method to check if an address is a stakeholder.
+    * @param _address The address to verify.
+    * @return bool, uint256 Whether the address is a stakeholder,
+    * and if so its position in the stakeholders array.
+    */
+   function isStakeholder(address _address)
+       public
+       view
+       returns(bool, uint256)
+   {
+       for (uint256 s = 0; s < stakeholders.length; s += 1){
+           if (_address == stakeholders[s]) return (true, s);
+       }
+       return (false, 0);
+   }   /**
+    * @notice A method to add a stakeholder.
+    * @param _stakeholder The stakeholder to add.
+    */
+   function addStakeholder(address _stakeholder)
+       public
+   {
+       (bool _isStakeholder, ) = isStakeholder(_stakeholder);
+       if(!_isStakeholder) stakeholders.push(_stakeholder);
+   }   /**
+    * @notice A method to remove a stakeholder.
+    * @param _stakeholder The stakeholder to remove.
+    */
+   function removeStakeholder(address _stakeholder)
+       public
+   {
+       (bool _isStakeholder, uint256 s) = isStakeholder(_stakeholder);
+       if(_isStakeholder){
+           stakeholders[s] = stakeholders[stakeholders.length - 1];
+           stakeholders.pop();
+       }
+   }
+   
+   
+   
+   /**
+    * @notice The stakes for each stakeholder.
+    */
+   mapping(address => uint256) internal stakes;
+   
+   
+   /**
+    * @notice A method to retrieve the stake for a stakeholder.
+    * @param _stakeholder The stakeholder to retrieve the stake for.
+    * @return uint256 The amount of wei staked.
+    */
+   function stakeOf(address _stakeholder)
+       public
+       view
+       returns(uint256)
+   {
+       return stakes[_stakeholder];
+   }   /**
+    * @notice A method to the aggregated stakes from all stakeholders.
+    * @return uint256 The aggregated stakes from all stakeholders.
+    */
+   function totalStakes()
+       public
+       view
+       returns(uint256)
+   {
+       uint256 _totalStakes = 0;
+       for (uint256 s = 0; s < stakeholders.length; s += 1){
+           _totalStakes = _totalStakes.add(stakes[stakeholders[s]]);
+       }
+       return _totalStakes;
+   }
+   
+   
 
+    /**
+    * @notice The accumulated rewards for each stakeholder.
+    */
+   mapping(address => uint256) internal rewards;
+  
+   /**
+    * @notice A method to allow a stakeholder to check his rewards.
+    * @param _stakeholder The stakeholder to check rewards for.
+    */
+   function rewardOf(address _stakeholder)
+       public
+       view
+       returns(uint256)
+   {
+       return rewards[_stakeholder];
+   }   /**
+    * @notice A method to the aggregated rewards from all stakeholders.
+    * @return uint256 The aggregated rewards from all stakeholders.
+    */
+   function totalRewards()
+       public
+       view
+       returns(uint256)
+   {
+       uint256 _totalRewards = 0;
+       for (uint256 s = 0; s < stakeholders.length; s += 1){
+           _totalRewards = _totalRewards.add(rewards[stakeholders[s]]);
+       }
+       return _totalRewards;
+   }
+   
+   
     function stake() public payable {
         emit Stake(msg.sender, msg.value);
         balances[msg.sender] += msg.value;
--- test/reward_test.js
+++ test/reward_test.js
@@ -5,18 +5,25 @@ const Staker = artifacts.require("Staker");
 
 var should = require('chai').should() 
 
+// Converting tokens from Ether to Wei (or similar metrics for this Token)
 function tokens(n) {
   return web3.utils.toWei(n, 'ether');
 }
 
-contract('Staker', (accounts) => {
+contract('RewardToken and Staker', (accounts) => {
   let RewardTokenContract, StakerContract
 
+
   before(async () => {
-    RewardTokenContract = await RewardToken.new(tokens('1000000'))
+    // Contracts Deployed
+    // 1 Billion RWT Tokens generated and deposited on msg.sender account
+    RewardTokenContract = await RewardToken.new(tokens('1000000000'))
     StakerContract = await Staker.new(RewardToken.address)
-    // Transfer all tokens to Staker (1 million)
-    await RewardTokenContract.transfer(StakerContract.address, tokens('1000000'))
+    // 997 Million RWT Tokens transfered to Staker Contract
+    await RewardTokenContract.transfer(StakerContract.address, tokens('997000000'))
+    // Transfer rest of tokens to different investors
+    await RewardTokenContract.transfer(accounts[1], tokens('1000000'))
+    await RewardTokenContract.transfer(accounts[2], tokens('1000000'))
   })
 
   describe('Contracts deployment', async () => {
@@ -27,12 +34,28 @@ contract('Staker', (accounts) => {
   })
 
   describe('Tokens in Staker Contract', async () => {
-    it('Stacker contract received 1Million tokens', async () => {
+    it('Stacker contract received 997 Million tokens', async () => {
         const balance = await RewardTokenContract.balanceOf(StakerContract.address)
-        assert.equal(balance, tokens('1000000'))
+        assert.equal(balance, tokens('997000000'))
     })
-
+    it('Investors received 1Million tokens each', async () => {
+        const balance = await RewardTokenContract.balanceOf(accounts[0])
+        assert.equal(balance, tokens('1000000'))
+        const balance1 = await RewardTokenContract.balanceOf(accounts[1])
+        assert.equal(balance1, tokens('1000000'))
+        const balance2 = await RewardTokenContract.balanceOf(accounts[2])
+        assert.equal(balance2, tokens('1000000'))
+    })
   })
 
+/*  describe('Staking', async () => {
+    it('Using stake function to deposit 5000 tokens', async () => {
+        // need to make sure this is transfering RWT tokens instead of Ether
+        await StakerContract.stake( { from: accounts[0] , value: tokens('5000') })
+       // const staked = await RewardTokenContract.balanceOf[ accounts[0] ]
+       // assert.equal( tokens(staked) , tokens('5000'))
+    })
+  })
+*/
 
 })
